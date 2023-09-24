3161eadb9d1a7dee03409fa8cc69e677a09552e9
ehuamanttupa <ehuamanttupa@stfonavi.gob.pe>
2023-09-24 02:47:06 -0500

    Singup API of User & Structure

---
+++ src/main/java/com/inn/cafe/CafeManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inn.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/inn/cafe/service/UserService.java
@@ -0,0 +1,12 @@
+package com.inn.cafe.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+
+public interface UserService {
+
+    ResponseEntity<String> signUp(Map<String,String> requestMap);
+}
