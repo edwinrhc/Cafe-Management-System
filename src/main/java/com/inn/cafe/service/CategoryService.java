c2958f63cc1c91b5d70cc103f1753da25c48ad80
ehuamanttupa <ehuamanttupa@stfonavi.gob.pe>
2023-10-11 00:00:59 -0500

    Avance del módulo Category

---
+++ src/main/java/com/inn/cafe/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.inn.cafe.service;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface CategoryService {
+
+    ResponseEntity<String> addNewCategory(Map<String, String> requestMap);
+
+}
