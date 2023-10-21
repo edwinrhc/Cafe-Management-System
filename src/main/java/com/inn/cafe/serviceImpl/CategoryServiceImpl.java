82d3ea79a3f517645092418316c6f26bde89f92e
ehuamanttupa <ehuamanttupa@stfonavi.gob.pe>
2023-10-20 23:39:03 -0500

    Finalizado módulo de Category

--- src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -1,18 +1,24 @@
 package com.inn.cafe.serviceImpl;
 
+import com.google.common.base.Strings;
 import com.inn.cafe.JWT.JwtFilter;
 import com.inn.cafe.POJO.Category;
 import com.inn.cafe.constents.CafeConstants;
 import com.inn.cafe.dao.CategoryDao;
 import com.inn.cafe.service.CategoryService;
 import com.inn.cafe.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+@Slf4j
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
@@ -40,6 +46,7 @@ public class CategoryServiceImpl implements CategoryService {
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+
     private boolean validateCategoryMap(Map<String, String> requestMap, boolean validateId) {
 
         if(requestMap.containsKey("name")){
@@ -60,4 +67,44 @@ public class CategoryServiceImpl implements CategoryService {
         category.setName(requestMap.get("name"));
         return category;
     }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
+        try {
+            if(!Strings.isNullOrEmpty(filterValue) && filterValue.equalsIgnoreCase("true")){
+                log.info("Inside if");
+                return new ResponseEntity<List<Category>>(categoryDao.getAllCategory(),HttpStatus.OK);
+            }
+            return new ResponseEntity<>(categoryDao.findAll(),HttpStatus.OK);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<List<Category>>(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
+        try{
+            if(jwtFilter.isAdmin()){
+                if(validateCategoryMap(requestMap,true)){
+                    Optional optional = categoryDao.findById(Integer.parseInt(requestMap.get("id")));
+                    if(!optional.isEmpty()){
+                            categoryDao.save(getCategoryFromMap(requestMap,true));
+                            return CafeUtils.getResponseEntity("Category Updated Successfully",HttpStatus.OK);
+                    }else{
+                      return CafeUtils.getResponseEntity("Category id does not existe",HttpStatus.OK);
+                    }
+                }
+                return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA,HttpStatus.BAD_REQUEST);
+            }else{
+                return CafeUtils.getResponseEntity(CafeConstants.UNAUTHORIZED_ACCESS,HttpStatus.UNAUTHORIZED);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return  CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
 }
