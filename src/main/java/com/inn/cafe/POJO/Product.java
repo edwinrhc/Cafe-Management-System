c3f47a175d765b248d109ba90ce6a94d35a61380
ehuamanttupa <ehuamanttupa@stfonavi.gob.pe>
2023-10-21 13:02:53 -0500

    Modulo finalizado de Add,Get,Uptade for Product

---
+++ src/main/java/com/inn/cafe/POJO/Product.java
@@ -0,0 +1,40 @@
+package com.inn.cafe.POJO;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NamedQuery(name = "Product.getAllProduct", query="select new com.inn.cafe.wrapper.ProductWrapper(p.id, p.name,p.description, p.price, p.status, p.category.id, p.category.name) from Product p")
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name="product")
+public class Product implements Serializable {
+
+    public static final Long serialVersionUid = 123456L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_fk",nullable = false)
+    private Category category;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="price")
+    private Integer price;
+
+    @Column(name="status")
+    private String status;
+}
---
+++ src/main/java/com/inn/cafe/restImpl/ProductRestImpl.java
@@ -0,0 +1,54 @@
+package com.inn.cafe.restImpl;
+
+import com.inn.cafe.constents.CafeConstants;
+import com.inn.cafe.rest.ProductRest;
+import com.inn.cafe.service.ProductService;
+import com.inn.cafe.utils.CafeUtils;
+import com.inn.cafe.wrapper.ProductWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ProductRestImpl implements ProductRest {
+
+    @Autowired
+    ProductService productService;
+
+    @Override
+    public ResponseEntity<String> addNewProduct(Map<String, String> requestMap) {
+        try {
+            return productService.addNewProduct(requestMap);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<ProductWrapper>> getAllProduct() {
+        try{
+            return productService.getAllProduct();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateProduct(Map<String, String> requestMap) {
+        try{
+            return productService.updateProduct(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    }
+
